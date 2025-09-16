e6c8cd56067f42e9651381e218c6b4e4760eddb0
Quallex <quallexl3t@gmail.com>
2025-09-16 16:17:16 +0200

    Update

--- addons/items/config.cpp
+++ addons/items/config.cpp
@@ -27,4 +27,36 @@ class CfgVehicles
 		maximumLoad=2000;
 		mass=1;
 	};
+		class TFAR_Invisible_Radio: B_Kitbag_rgr
+	{
+		author="Quallex";
+		scope=2;
+		displayName="TFAR Invisible Radio";
+		picture="TFAR_Invisible_Radio\data\icon\TIS_LOGO.paa";
+		model="\TFAR_Invisible_Radio\Hide.p3d";
+		maximumLoad=0;
+		mass=0;
+		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio=1;
+		tf_range=20000;
+		tf_encryptionCode="tf_west_radio_code";
+		tf_dialog="rt1523g_radio_dialog";
+		tf_subtype="digital_lr";
+	};
+	class TFAR_Invisible_Radio_Zeus: B_Kitbag_rgr
+	{
+		author="Quallex";
+		scope=2;
+		displayName="TFAR Invisible Radio [TYLKO ZEUS]";
+		picture="TFAR_Invisible_Radio\data\icon\TIS_LOGO.paa";
+		model="\TFAR_Invisible_Radio\Hide.p3d";
+		maximumLoad=5000;
+		mass=0;
+		tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio=1;
+		tf_range=100000;
+		tf_encryptionCode="tf_west_radio_code";
+		tf_dialog="rt1523g_radio_dialog";
+		tf_subtype="digital_lr";
+	};
 };
